a74de64455636cd5d5f410fe72d3b9044f27b5ae
Dor-J <Mr.DorJosef@gmail.com>
2025-01-22 15:27:28 +0200

    Add mail preview

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,5 +1,58 @@
 // apps/mail/cmps/MailPreview.jsx
 
-export function MailList() {
-  return <div>Mail list</div>
+/**
+ * [] Present a mail preview
+ * [] Renders the subject (with text size limit)
+ * [] Gives visual indication for read/unread
+ * [] Support hover state
+ */
+
+export function MailPreview({ mail }) {
+  const {
+    id,
+    subject,
+    createdAt,
+    updatedAt,
+    body,
+    isRead,
+    isStared,
+    sentAt,
+    removedAt,
+    from,
+    to,
+  } = mail
+  return (
+    <article className='mail-preview'>
+      <div className='mail-card-select-checkbox'>
+        <input
+          // onChange={handleChange}
+          // checked={isOnSale}
+          id={`mail-checked-${id}`}
+          type='checkbox'
+          name={`mail-checked-${id}`}
+        />
+      </div>
+      <div className='mail-card-select-starred'>
+        <span className='mail-card-title'>{isStared ? '⭐' : '★'}</span>
+      </div>
+      <div className='mail-card-select-sender'>
+        <span className='mail-card-title'>{from}</span>
+      </div>
+
+      <div className='mail-card-main'>
+        <div className='mail-card-select-subject'>
+          <span className='mail-card-subject'>{subject}</span>
+        </div>
+        <div className='mail-card-select-body'>
+          <span className='mail-card-body'>{body}</span>
+        </div>
+      </div>
+
+      <div className='mail-card-details'>
+        <div className='mail-card-date'>
+          <span className='mail-card-details-date'>{createdAt.toDate()}</span>
+        </div>
+      </div>
+    </article>
+  )
 }
