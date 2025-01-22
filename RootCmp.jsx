d107a76f274aa4edecf4391d8c5d16ed9be182de
Dor-J <Mr.DorJosef@gmail.com>
2025-01-22 20:13:58 +0200

    Add compose  to root

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -5,7 +5,9 @@ import { AppHeader } from './cmps/AppHeader.jsx'
 import { About } from './pages/About.jsx'
 import { Home } from './pages/Home.jsx'
 import { NoteIndex } from './apps/note/pages/NoteIndex.jsx'
+
 import { MailIndex } from './apps/mail/pages/MailIndex.jsx'
+import { MailCompose } from './apps/mail/pages/MailCompose.jsx'
 import { MailDetails } from './apps/mail/pages/MailDetails.jsx'
 
 export function App() {
@@ -20,8 +22,8 @@ export function App() {
 
           <Route path='/mail' element={<MailIndex />}>
             <Route path='/mail/:mailId' element={<MailDetails />} />
-            {/* <Route path='/mail/compose' element={<MailCompose />} />
-              <Route path='/mail/compose/:mailId' element={<MailCompose />} /> */}
+            <Route path='/mail/compose' element={<MailCompose />} />
+            <Route path='/mail/compose/:mailId' element={<MailCompose />} />
           </Route>
 
           <Route path='/note' element={<NoteIndex />} />
