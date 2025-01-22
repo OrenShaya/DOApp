6e3b2e94512be4978642c687695ea9018fab894a
Dor-J <Mr.DorJosef@gmail.com>
2025-01-22 17:37:12 +0200

    Add css

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -25,20 +25,22 @@ export function MailPreview({ mail }) {
   } = mail
   return (
     <article className='mail-preview'>
-      <div className='mail-card-select-checkbox'>
-        <input
-          // onChange={handleChange}
-          // checked={isOnSale}
-          id={`mail-checked-${id}`}
-          type='checkbox'
-          name={`mail-checked-${id}`}
-        />
-      </div>
-      <div className='mail-card-select-starred'>
-        <span className='mail-card-title'>{isStared ? '⭐' : '★'}</span>
-      </div>
-      <div className='mail-card-select-sender'>
-        <span className='mail-card-title'>{from.name}</span>
+      <div className='mail-card-main-select'>
+        <div className='mail-card-select-checkbox'>
+          <input
+            // onChange={handleChange}
+            // checked={isOnSale}
+            id={`mail-checked-${id}`}
+            type='checkbox'
+            name={`mail-checked-${id}`}
+          />
+        </div>
+        <div className='mail-card-select-starred'>
+          <span className='mail-card-title'>{isStared ? '⭐' : '★'}</span>
+        </div>
+        <div className='mail-card-select-sender'>
+          <span className='mail-card-title'>{from.name}</span>
+        </div>
       </div>
 
       <div className='mail-card-main'>
