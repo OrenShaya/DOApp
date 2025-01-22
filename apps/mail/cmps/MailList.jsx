f5e83ed3a7b8eb5d8272a2cb76a0b58aef3444e5
Dor-J <Mr.DorJosef@gmail.com>
2025-01-22 15:26:24 +0200

    Add mail list cmpt

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,5 +1,32 @@
 // apps/mail/cmps/MailList.jsx
 
-export function MailList() {
-  return <div>Mail list</div>
+/**
+ * [] Renders a list of <MailPreview> pass down a mail prop
+ */
+
+const { Link } = ReactRouterDOM
+
+import { MailPreview } from './MailPreview.jsx'
+
+export function MailList({ mails, onRemoveMail }) {
+  return (
+    <section className='mails-list'>
+      <ul className='mail-list'>
+        {mails.map((mail) => (
+          <li key={mail.id}>
+            <MailPreview mail={mail} />
+            <div className='btns-container'>
+              <button onClick={() => onRemoveMail(mail.id)}>Remove Mail</button>
+              <button>
+                <Link to={`/mail/${mail.id}`}>Open</Link>
+              </button>
+              {/* <button>
+                <Link to={`/mail/edit/${mail.id}`}>Edit</Link>
+              </button> */}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
 }
