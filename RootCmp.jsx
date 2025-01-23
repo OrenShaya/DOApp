eaaf8043b8662e3e9b4454785e0983db6cf02353
Dor-J <Mr.DorJosef@gmail.com>
2025-01-23 13:52:51 +0200

    Setup rootCmp

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -4,10 +4,11 @@ const Router = ReactRouterDOM.HashRouter
 import { AppHeader } from './cmps/AppHeader.jsx'
 import { About } from './pages/About.jsx'
 import { Home } from './pages/Home.jsx'
+import { UserMsg } from './cmps/UserMsg.jsx'
 import { NoteIndex } from './apps/note/pages/NoteIndex.jsx'
 
 import { MailIndex } from './apps/mail/pages/MailIndex.jsx'
-import { MailCompose } from './apps/mail/pages/MailCompose.jsx'
+import { MailCompose } from './apps/mail/cmps/MailCompose.jsx'
 import { MailDetails } from './apps/mail/pages/MailDetails.jsx'
 import { NoteDetail } from './apps/note/cmps/NotesDetail.jsx'
 
@@ -22,16 +23,17 @@ export function App() {
           <Route path='/about' element={<About />} />
 
           <Route path='/mail' element={<MailIndex />}>
-            <Route path='/mail/:mailId' element={<MailDetails />} />
             <Route path='/mail/compose' element={<MailCompose />} />
-            <Route path='/mail/compose/:mailId' element={<MailCompose />} />
           </Route>
+          <Route path='/mail/:mailId' element={<MailDetails />} />
+          <Route path='/mail/compose/:mailId' element={<MailCompose />} />
 
           <Route path='/note' element={<NoteIndex />}>
-              <Route path='/note/:noteId' element={<NoteDetail />} />
+            <Route path='/note/:noteId' element={<NoteDetail />} />
           </Route>
         </Routes>
       </section>
+      <UserMsg />
     </Router>
   )
 }
