6ca5b0847adacebb6bd05e238c8cd116e54a3d87
Dor-J <Mr.DorJosef@gmail.com>
2025-01-23 16:36:14 +0200

    Move routes

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -5,7 +5,6 @@ import { AppHeader } from './cmps/AppHeader.jsx'
 import { About } from './pages/About.jsx'
 import { Home } from './pages/Home.jsx'
 import { UserMsg } from './cmps/UserMsg.jsx'
-import { NoteIndex } from './apps/note/pages/NoteIndex.jsx'
 
 import { MailIndex } from './apps/mail/pages/MailIndex.jsx'
 import { MailCompose } from './apps/mail/cmps/MailCompose.jsx'
@@ -27,9 +26,9 @@ export function App() {
 
           <Route path='/mail' element={<MailIndex />}>
             <Route path='/mail/compose' element={<MailCompose />} />
+            <Route path='/mail/compose/:mailId' element={<MailCompose />} />
           </Route>
           <Route path='/mail/:mailId' element={<MailDetails />} />
-          <Route path='/mail/compose/:mailId' element={<MailCompose />} />
 
           <Route path='/note' element={<NoteIndex />} />
           <Route path='/note/:noteId' element={<NoteDetail />} />
