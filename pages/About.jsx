a59d46d71e06c61114c4c18b5023295b993a3271
Dor-J <Mr.DorJosef@gmail.com>
2025-01-25 20:42:12 +0200

    Add about section

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,7 +1,47 @@
 export function About() {
   return (
     <section className='about'>
-      <h1>About Page</h1>
+      <div className='about-container'>
+        <h1>About Page</h1>
+        <h2>Our Team</h2>
+        <article className='team-profile'>
+          <div className='profile-image-container'>
+            <img
+              src='../assets/img/profile/dor-profile-pic.jpeg'
+              alt='dor-profile-pic'
+            />
+          </div>
+          <div className='profile-about-me'>
+            <h3>Dor Josef</h3>
+            <p>
+              Hi, I'm Dor, a Full-Stack developer with a passion for creating
+              sleek, responsive web apps using React and MVC architecture. I
+              follow a mobile-first approach to CSS and have a sharp eye for
+              quality, ensuring every project is clean, efficient, and
+              user-friendly. Also I have a B.Sc for Food and Biotechnology
+              engineering and experiance with more programming languages
+            </p>
+          </div>
+        </article>
+        <article className='team-profile'>
+          <div className='profile-image-container'>
+            <img
+              src='../assets/img/profile/oren-profile-pic.jpeg'
+              alt='oren-profile-pic'
+            />
+          </div>
+          <div className='profile-about-me'>
+            <h3>Oren Shaya</h3>
+            <p>
+              Hi, I'm Oren, a Full-Stack developer with motivation for making
+              great ideas come to life espcially on web. I'm fully equipped with
+              knowledge of JavaScript, React, HTML, CSS using the MVC
+              architecture. I also hold a Bachlore degree on Software
+              Engineering and have knowledge on other programming languages.
+            </p>
+          </div>
+        </article>
+      </div>
     </section>
   )
 }
