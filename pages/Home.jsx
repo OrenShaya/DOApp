76f829e2768d3d9b7bbc1fdf8ef8c6e09a0f87b9
Dor-J <Mr.DorJosef@gmail.com>
2025-01-26 02:13:15 +0200

    Add home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,7 +1,35 @@
+const { useNavigate } = ReactRouterDOM
+
 export function Home() {
+  const navigate = useNavigate()
+
   return (
     <section className='home'>
-      <h1>Welcome to home page!</h1>
+      <div className='home-header'>
+        <span>DO App</span>
+      </div>
+      <div className='welcome-message'>
+        <p>
+          Welcome to DO app where we DO it differently with Dor & Oren <br />{' '}
+          Check out our mail app and notes app
+        </p>
+      </div>
+      <div className='navigation-btns'>
+        <button
+          type='button'
+          className='home-nav-btn'
+          onClick={() => navigate('/mail/')}
+        >
+          Mail
+        </button>
+        <button
+          type='button'
+          className='home-nav-btn'
+          onClick={() => navigate('/note/')}
+        >
+          Note
+        </button>
+      </div>
     </section>
   )
 }
